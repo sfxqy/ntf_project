a106daaefd2da0330a28b5cf1de7d9e4c6db1c75
SFX <1273152914@qq.com>
2022-04-27 23:06:28 +0800

    j

--- src/main/java/com/ntf/ntf_project/constants/IBoxURL.java
+++ src/main/java/com/ntf/ntf_project/constants/IBoxURL.java
@@ -9,6 +9,7 @@ public class IBoxURL {
   /**
    * 公告地址
    */
+  public static String NOTICE_PAGE_URL = "https://www.ibox.art/zh-cn/notice/";
   public static String NOTICE_URL = "https://api-h5.ibox.art/nft-mall-web/v1.2/nft/product/getCommonNoticeList?page=1&pageSize=20";
 
   public static  String s="https://api-h5.ibox.art/nft-mall-web/v1.2/nft/product/getCommonNoticeList?page=1&pageSize=20";
--- src/main/java/com/ntf/ntf_project/service/ibox/IBoxDataService.java
+++ src/main/java/com/ntf/ntf_project/service/ibox/IBoxDataService.java
@@ -15,6 +15,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Handler;
 import javax.annotation.Resource;
 import org.apache.http.client.HttpClient;
@@ -23,6 +24,10 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -41,6 +46,102 @@ public class IBoxDataService {
   private RestTemplateUtil restTemplateUtil;
   @Resource
   private RestTemplate restTemplate;
+
+
+  public static void main(String[] args)throws Exception {
+    IBoxDataService iBoxDataService = new IBoxDataService();
+    iBoxDataService.login();
+  }
+
+  public void login()throws Exception{
+//		配置本地浏览器驱动路径
+    System.getProperties().setProperty("webdriver.chrome.driver", "C:\\Users\\SFX\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+//		创建浏览器对象
+    ChromeDriver chromeDriver = new ChromeDriver();
+//		设置浏览器窗口最大化
+    chromeDriver.manage().window().maximize();
+//		设置需要操作的网页url
+    chromeDriver.get("https://www.ibox.art/zh-cn/");
+//		设置隐式等待时间，根据目标网页的响应速度设置超时时间
+    chromeDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    chromeDriver.findElement(By.xpath("//*[@id=\"__layout\"]/section/header/div[1]/div[5]/div[1]")).click();
+
+    //滑块
+   // WebElement  slider= chromeDriver.findElement(By.cssSelector("#nc_1_n1z"));
+    WebElement  slider= chromeDriver.findElementByCssSelector("#nc_1_n1z");
+
+    //滑动槽
+    //WebElement element = chromeDriver.findElement(By.cssSelector("#nc_1_n1t"));
+    WebElement element = chromeDriver.findElementByCssSelector("#nc_1__scale_text");
+    element.getSize().getWidth();
+    element.getSize().getHeight();
+    Actions actions = new Actions(chromeDriver);
+    actions.dragAndDropBy(slider,element.getSize().getWidth()/2,element.getSize().getHeight()).perform();
+    Thread.sleep(2000);
+    actions.dragAndDropBy(slider,element.getSize().getWidth()+1,element.getSize().getHeight()).perform();
+
+  /*  chromeDriver.findElement(By.cssSelector("##__layout > section > header > div.inner > div.wallet-button > div:nth-child(1)"))
+    for (String pho : phoNumList) {
+      try {
+        //防止访问太快休息一秒
+        Thread.sleep(1000l);
+//		使用css定位手机号的输入框
+        WebElement inputPhoNum = chromeDriver.findElement(By.cssSelector("form>section>input"));
+//		清除输入框内容
+        inputPhoNum.clear();
+//		输入手机号
+        inputPhoNum.sendKeys(pho);
+//		休息一秒
+        Thread.sleep(1000l);
+//		使用css定位获取验证码的按钮并完成点击
+        chromeDriver.findElement(By.cssSelector("form>section>button")).click();
+//		这时js会加载拖动验证的滑块，休息两秒
+        Thread.sleep(2000l);
+//		使用ID定位js加载出来的滑动验证
+        WebElement slideCode = chromeDriver.findElementById("nc_1_n1z");
+//		获取节点坐标对象
+        Point point = slideCode.getLocation();
+//		创建按键模拟的对象
+        Actions action = new Actions(chromeDriver);
+        Thread.sleep(1000l);
+//		模拟鼠标点击拖动滑块完成验证
+        action.dragAndDropBy(slideCode, point.x + 170, point.y).perform();
+//		使用封装的获取短信验证码的工具
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("PhoNum", pho);
+        String smsCode = getSmsCode(map);
+//		判断是否获取到短信验证码，否则切换手机号
+        if (smsCode == "" || smsCode == null) {
+          continue;
+        }
+//		使用cssh定位短信验证码输入框
+        Thread.sleep(1000l);
+        WebElement vCode = chromeDriver.findElement(By.cssSelector("[placeholder=验证码]"));
+//		输入短信验证码
+        vCode.sendKeys(smsCode);
+        Thread.sleep(1000l);
+//		使用class定位登录按钮位置
+        WebElement loginBtn = chromeDriver.findElementByClassName("SubmitButton-2wG4T");
+//		点击登录
+        loginBtn.click();
+//		获取已登录的cookie
+        Set<Cookie> cookies = chromeDriver.manage().getCookies();
+        for (Cookie ck : cookies) {
+          System.out.println(ck);
+        }
+
+      } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }finally {
+//		所有操作执行完成，关闭浏览器
+        chromeDriver.close();
+      }
+    }
+*/
+
+  }
 
   /**
    * 获取IBox公告信息
@@ -72,7 +173,7 @@ public class IBoxDataService {
 
   public void getIboxNotice1()throws Exception{
     CloseableHttpClient httpClient = HttpClients.createDefault();
-    HttpGet httpGet = new HttpGet(IBoxURL.NOTICE_URL);
+    HttpGet httpGet = new HttpGet("https://api-h5.ibox.art/nft-mall-web/v1.2/nft/product/getNoticeList");
     CloseableHttpResponse response = null;
     try {
       response = httpClient.execute(httpGet);
@@ -90,6 +191,9 @@ public class IBoxDataService {
     }
 
   }
+
+
+
 
 
 }
